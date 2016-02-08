17d0b6c722aeef4590403c94e2f32ee2c84a5afb
Mathieu Virbel <mat@kivy.org>
2016-02-08 17:05:54 +0100

    new approach working for kivy 1.9

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
 from kivy.app import App
 from kivy.properties import StringProperty
+from kivy.lang import Observable, EventDispatcher
 from os.path import join, dirname
 import gettext
 
-class _(str):
 
+class Lang(Observable):
     observers = []
     lang = None
 
-    def __new__(cls, s, *args, **kwargs):
-        if _.lang is None:
-            _.switch_lang('en')
-        s = _.translate(s, *args, **kwargs)
-        return super(_, cls).__new__(cls, s)
+    def __init__(self, defaultlang):
+        super(Lang, self).__init__()
+        self.ugettext = None
+        self.lang = defaultlang
+        self.switch_lang(self.lang)
 
-    @staticmethod
-    def translate(s, *args, **kwargs):
-        return _.lang(s).format(args, kwargs)
+    def _(self, text):
+        return self.ugettext(text)
 
-    @staticmethod
-    def bind(**kwargs):
-        _.observers.append(kwargs['_'])
+    def fbind(self, name, func, args, **kwargs):
+        if name == "_":
+            self.observers.append((func, args, kwargs))
+        else:
+            return super(Lang, self).fbind(name, func, *largs, **kwargs)
 
-    @staticmethod
-    def switch_lang(lang):
+    def funbind(self, name, func, args, **kwargs):
+        if name == "_":
+            key = (func, args, kwargs)
+            if key in self.observers:
+                self.observers.remove(key)
+        else:
+            return super(Lang, self).funbind(name, func, *args, **kwargs)
+
+    def switch_lang(self, lang):
         # get the right locales directory, and instanciate a gettext
         locale_dir = join(dirname(__file__), 'data', 'locales')
-        locales = gettext.translation('langapp', locale_dir,
-            languages=[lang])
-        _.lang = locales.ugettext
+        locales = gettext.translation('langapp', locale_dir, languages=[lang])
+        self.ugettext = locales.ugettext
 
         # update all the kv rules attached to this text
-        for callback in _.observers:
-            callback()
+        for func, largs, kwargs in self.observers:
+            func(largs, None, None)
+
+
+tr = Lang("en")
 
 
 class LangApp(App):
@@ -40,7 +51,7 @@ class LangApp(App):
     lang = StringProperty('en')
 
     def on_lang(self, instance, lang):
-        _.switch_lang(lang)
+        tr.switch_lang(lang)
 
 
 LangApp().run()
